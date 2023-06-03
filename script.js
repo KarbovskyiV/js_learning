28408f90ee56f86eb8ab859c24c9390f50c176fd
Smash2222 <karbovskyi.volodymyr@gmail.com>
2023-06-03 21:06:45 +0300

    add bigint and symbol type of data

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert(123123);
+// alert(123123);
+
+const bigInt = 9999999999999999n;
+console.log(bigInt);
+console.log(typeof bigInt);
+
+const user = {
+    id: 10,
+    age: 20,
+    height: 170,
+}
+
+const id = Symbol('Some new symbol');
+user[id] = 500;
+console.log(user);
+console.log(typeof id);
